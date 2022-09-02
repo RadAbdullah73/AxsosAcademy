f88fb316d4fb241498b04d4b41c62036d8db86f4
Raed Abdullah <raedabdullah733@gmail.com>
2022-09-02 19:27:16 +0300

    calculator

--- Web Fundemntals/Java Script/Calculater/call.js
+++ Web Fundemntals/Java Script/Calculater/call.js
@@ -8,13 +8,16 @@ function press(n) {
     if (clear.innerText == 0) {
         clear.innerText = "";
     }
+    if (clear.innerText == x ){ //to keep x visible on display screen . 
+        clear.innerText = ""
+    }
     clear.innerText = clear.innerText + n; //to input mora than one digit on display screen .  
     num1 = Number(clear.innerText); // here Number convert the text to number .  
     return num1;
 }
 function setOP(t) {
     x = Number(clear.innerText); // ---> x = num1 . 
-    clear.innerText = "";
+    clear.innerText = num1 ;
     t1 = t;
     return t1;
 }
