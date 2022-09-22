c8b171a8a87e82d5b3f9315b680b73acea7c41ac
Raed Abdullah <raedabdullah733@gmail.com>
2022-09-22 13:54:56 +0300

    Add users Assignment

--- Python_stack/DjangoAssignment/Users/UsersApp/views.py
+++ Python_stack/DjangoAssignment/Users/UsersApp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render , redirect
+from .models import User
+def index(request):
+    context = {
+    "all_the_Users": User.objects.all()
+    }
+    return render(request, "index.html", context)
+def show(request):
+    User.objects.create(username= request.POST['firstname'] , lastname=request.POST['lastname'] , email=request.POST['Email'] , Age=request.POST['Age']) 
+    return redirect('/')
