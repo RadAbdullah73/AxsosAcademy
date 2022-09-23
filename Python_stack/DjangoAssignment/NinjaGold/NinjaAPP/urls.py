abc4403bca1f709102fa6a255a4dd14d659f4239
Raed Abdullah <raedabdullah733@gmail.com>
2022-09-23 19:28:11 +0300

    F

--- Python_stack/DjangoAssignment/NinjaGold/NinjaAPP/urls.py
+++ Python_stack/DjangoAssignment/NinjaGold/NinjaAPP/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
-    # path('increasegolds' , views.increasing),
+    path('increasegolds/' , views.increasing),
+    path('incORdec/' ,views.increasingORdecreasing) 
 ]
--- Python_stack/DjangoAssignment/NinjaGold/NinjaAPP/views.py
+++ Python_stack/DjangoAssignment/NinjaGold/NinjaAPP/views.py
@@ -1,7 +1,41 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+import random
 def index(request):
     return render ( request , 'index.html')
+def increasing(request):
+    RandomNumber = random.randint(10, 20)
+    if 'gold' in request.session:
+        request.session['gold']+= RandomNumber
+    else:
+        request.session['gold']= RandomNumber
+    if request.POST['which_form']== "Cave":
+        request.session['Activity'] = 'You Entered a Cave and earned' , RandomNumber , 'golds'
+        # Array.append(request.session['Activity'])
+    elif request.POST['which_form']== "Farm":
+        request.session['Activity'] = 'You Entered a Farm and earned' , RandomNumber , 'golds'
+        # Array.append(request.session['Activity'])
+    elif request.POST['which_form']== "House":
+        request.session['Activity'] = 'You Entered a House and earned' , RandomNumber , 'golds'
+        # Array.append(request.session['Activity'])
+    return redirect('/')
+
+def increasingORdecreasing(request):
+        RandomNumber = random.randint(0, 50)
+        RandomOperation=random.choice(['+' , '-'])
+        if 'gold' in request.session:
+            if RandomOperation=='+':
+                request.session['gold'] += RandomNumber
+                request.session['Activity'] = 'You Entered a Quest and earned' , RandomNumber , 'golds'
+                # Array.append(request.session['Activity'])
+            else:
+                request.session['gold'] -= RandomNumber
+                request.session['Activity'] = 'You Entered a Quest and Lost' , RandomNumber , 'Golds'
+                # Array.append(request.session['Activity'])
+        else:
+            request.session['gold']= RandomNumber
+        return redirect('/')
+        
 
 
-# def increasing(request):
+
+
