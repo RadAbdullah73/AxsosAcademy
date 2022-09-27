ebea92f505c55af847d8020e333a2ea4223e8e41
Rad_Abdullah <raedabdullah733@gmail.com>
2022-09-27 16:56:07 +0300

    Assignment: Books/Authors with Templates

--- Python_stack/DjangoAssignment/Authors&books/AuthorsAndBooks/App/urls.py
+++ Python_stack/DjangoAssignment/Authors&books/AuthorsAndBooks/App/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', index.views),
+    path('', views.index),
+    path('addBook' , views.AddBook),
+    path('authors', views.index1),
+    path('addPublisher' , views.addPublisher),
+    path('goToPublisher' , views.Go),
+    path('goToBooks' , views.Back),
+    path('books/<int:id>' , views.BookView),
+    path('addPupToBook/<int:id>' , views.addPupToBook),
+    path('publishers/<int:id>', views.PupView),
+    path('addBookToPup/<int:id>' ,views.addBookToPup),
 ]
--- Python_stack/DjangoAssignment/Authors&books/AuthorsAndBooks/App/views.py
+++ Python_stack/DjangoAssignment/Authors&books/AuthorsAndBooks/App/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from contextlib import redirect_stderr
+from multiprocessing import context
+from django.shortcuts import render , redirect
+from .models import *
 
-# Create your views here.
+def index(request):
+    context={
+        'books':Book.objects.all()
+    }
+    return render (request , 'index.html' , context)
+
+def AddBook(request):
+    Book.objects.create(title =request.POST['title'] , desc = request.POST['desc'])
+    return redirect('/')
+
+def index1(request):
+    context={
+        'publishers':Publisher.objects.all()
+    }
+    return render (request , 'index1.html' , context)
+
+def addPublisher(request):
+    Publisher.objects.create(first_name =request.POST['first_name'] , last_name = request.POST['last_name'])
+    return redirect('/authors')
+
+def Go(request):
+    return redirect ('/authors')
+
+def BookView(request , id):
+    context={
+        'oneBook' : Book.objects.get(id = int(id)),
+        'Publishers' : Publisher.objects.all()
+    }
+    return render ( request , 'index3.html' , context)
+
+def addPupToBook(request,id):
+    book = Book.objects.get(id = int(id))
+    Pub = Publisher.objects.get(id=request.POST['selectAuthor'])
+    book.publishers.add(Pub)
+    return redirect ('/books' +'/' +str(id))
+
+def PupView(request , id):
+    context = {
+        'firstPup' : Publisher.objects.get(id=int(id)),
+        'Books' : Book.objects.all()
+    }
+    return render ( request , 'index4.html' , context)
+
+def addBookToPup(request,id):
+    pub = Publisher.objects.get(id = int(id))
+    book = Book.objects.get(id=request.POST['selectBook'])
+    pub.books.add(book)
+    return redirect ('/publishers' +'/' +str(id))
+
+def Back(request):
+    return redirect ('/')
+
