8227573f64e1514de8c3490608531755f92109b8
Rad_Abdullah <raedabdullah733@gmail.com>
2022-12-06 17:36:20 +0300

    First Day of React Assginments

--- MERN/JavaScript (NodeJs)/Object Master/objectMaster.js
+++ MERN/JavaScript (NodeJs)/Object Master/objectMaster.js
@@ -18,28 +18,28 @@ const pokémon = Object.freeze([
         { "id": 115, "name": "Kangaskhan", "types": ["normal"] },
         { "id": 122, "name": "Mr. Mime",   "types": ["psychic"] },
         { "id": 133, "name": "Eevee",      "types": ["normal"] },
-        { "id": 144, "name": "Articuno",   "types": ["ice", "flying"] },
+        { "id": 144, "name": "Articuno",   "types": ["ice","flying"] },
         { "id": 145, "name": "Zapdos",     "types": ["electric", "flying"] },
         { "id": 146, "name": "Moltres",    "types": ["fire", "flying"] },
         { "id": 148, "name": "Dragonair",  "types": ["dragon"] }
     ]);
 
-    const bListPkmn = pokémon.filter( p => p.name[0] === "B" );
-    console.log(bListPkmn);
-    const bListPkmn1 = pokémon.filter( p => p.id % 3 ==0 );
-    console.log(bListPkmn1);
-    const bListPkmn2 = pokémon.filter( p => p.types.includes("fire"));
-    console.log(bListPkmn2);
-    const bListPkmn3 = pokémon.filter( p => p.types.length >=2);
-    console.log(bListPkmn3);
-    const bListPkmn4 = pokémon.map( p => p.name);
-    console.log(bListPkmn4);
-    const bListPkmn5 = pokémon.filter(b => b.id >99).map( p => p.name);
-    console.log(bListPkmn5);
-    const bListPkmn6= pokémon.filter( p => p.types=="poison");
-    console.log(bListPkmn6);
-    const bListPkmn7= pokémon.filter( p => p.types.includes("flying")).map(n=>n.types[0]);
-    console.log(bListPkmn7);
+    // const bListPkmn = pokémon.filter( p => p.name[0] === "B" );
+    // console.log(bListPkmn);
+    // const bListPkmn1 = pokémon.filter( p => p.id % 3 ==0 );
+    // console.log(bListPkmn1);
+    // const bListPkmn2 = pokémon.filter( p => p.types.includes("fire"));
+    // console.log(bListPkmn2);
+    // const bListPkmn3 = pokémon.filter( p => p.types.length >=2);
+    // console.log(bListPkmn3);
+    // const bListPkmn4 = pokémon.map( p => p.name);
+    // console.log(bListPkmn4);
+    // const bListPkmn5 = pokémon.filter(b => b.id >99).map( p => p.name);
+    // console.log(bListPkmn5);
+    // const bListPkmn6= pokémon.filter( p => p.types=="poison");
+    // console.log(bListPkmn6);
+    // const bListPkmn7= pokémon.filter( p => p.types[1]=="flying").map(n=>n.types[0]);
+    // console.log(bListPkmn7);
     const bListPkmn8= pokémon.filter( p => p.types.includes("normal")).length;
     console.log(bListPkmn8);
 
